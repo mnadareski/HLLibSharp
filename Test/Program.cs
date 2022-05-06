3a76ad3e250a2040a118311307ad15f347eeb828
Matt Nadareski <mnadareski@outlook.com>
2022-05-06 13:56:57 -0700

    Add framework to test program

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using HLLib;
+using HLLib.Directory;
+using HLLib.Packages;
 
 namespace Test
 {
@@ -10,7 +9,54 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            // Do nothing
+            foreach (string arg in args)
+            {
+                Console.WriteLine($"Path: {arg}");
+
+                // Derive the type from the file
+                PackageType packageType = DeriveType(arg);
+                Console.WriteLine($"Package Type: {packageType}");
+                if (packageType == PackageType.HL_PACKAGE_NONE)
+                    continue;
+
+                // Create a new package from the file
+                Package package = Package.CreatePackage(packageType);
+                bool opened = package.Open(arg, FileModeFlags.HL_MODE_READ | FileModeFlags.HL_MODE_WRITE, overwriteFiles: true);
+                if (!opened)
+                {
+                    Console.WriteLine("Package could not be opened!");
+                    continue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Derive the type for a file safely
+        /// </summary>
+        /// <param name="file">File path to check</param>
+        /// <returns>Internal package type of the file</returns>
+        private static PackageType DeriveType(string file)
+        {
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"File {file} does not exist, skipping...");
+                return PackageType.HL_PACKAGE_NONE;
+            }
+
+            try
+            {
+                using (FileStream fs = File.OpenRead(file))
+                {
+                    byte[] buffer = new byte[16];
+                    fs.Read(buffer, 0, 16);
+                    return Package.GetPackageTypeFromHeader(buffer);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return PackageType.HL_PACKAGE_NONE;
+            }
         }
     }
 }
