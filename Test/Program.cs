1c27f9178dd7428e6c19d0212b786dd9cbf8a437
Matt Nadareski <mnadareski@outlook.com>
2022-05-06 13:45:27 -0700

    Add empty test project

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using HLLib;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do nothing
+        }
+    }
+}
