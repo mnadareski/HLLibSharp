910c065569f34d42d855bc9c00595aa92a88071f
Matt Nadareski <mnadareski@outlook.com>
2022-05-07 23:02:12 -0700

    Handle directories in test program

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,49 +11,73 @@ namespace Test
         {
             foreach (string arg in args)
             {
-                try
+                if (Directory.Exists(arg))
                 {
-                    Console.WriteLine($"Path: {arg}");
-
-                    // Derive the type from the file
-                    PackageType packageType = DeriveType(arg);
-                    if (packageType == PackageType.HL_PACKAGE_NONE)
+                    foreach (string file in Directory.EnumerateFiles(arg))
                     {
-                        Console.WriteLine("Could not determine package type!");
-                        Console.WriteLine();
-                        continue;
+                        ProcessFile(file);
                     }
-
-                    // Create a new package from the file
-                    Package package = Package.CreatePackage(packageType);
-                    FileModeFlags mode = FileModeFlags.HL_MODE_READ | FileModeFlags.HL_MODE_WRITE | FileModeFlags.HL_MODE_NO_FILEMAPPING;
-                    bool opened = package.Open(arg, mode, overwriteFiles: true);
-                    if (!opened)
-                    {
-                        Console.WriteLine("Package could not be opened!");
-                        Console.WriteLine();
-                        continue;
-                    }
-
-                    // Print all package info
-                    Package.PrintPackageInfo(package);
-
-                    // Create the root directory
-                    DirectoryFolder rootDirectory = package.GetRoot();
-
-                    // Extract all files
-                    string outputPath = Path.Combine(Path.GetDirectoryName(Path.GetFullPath(arg)), Path.GetFileNameWithoutExtension(arg));
-                    rootDirectory.Extract(outputPath, readEncrypted: true, overwrite: true);
-
-                    // Close the package explicitly
-                    package.Close();
+                }
+                else if (File.Exists(arg))
+                {
+                    ProcessFile(arg);
+                }
+                else
+                {
+                    Console.WriteLine($"{arg} is not a file or folder, skipping...");
                     Console.WriteLine();
                 }
-                catch (Exception ex)
+            }
+        }
+
+        /// <summary>
+        /// Process a single file
+        /// </summary>
+        /// <param name="file">Path to a single file</param>
+        private static void ProcessFile(string file)
+        {
+            try
+            {
+                Console.WriteLine($"Path: {file}");
+
+                // Derive the type from the file
+                PackageType packageType = DeriveType(file);
+                if (packageType == PackageType.HL_PACKAGE_NONE)
                 {
-                    Console.WriteLine(ex);
+                    Console.WriteLine("Could not determine package type!");
                     Console.WriteLine();
+                    return;
                 }
+
+                // Create a new package from the file
+                Package package = Package.CreatePackage(packageType);
+                FileModeFlags mode = FileModeFlags.HL_MODE_READ | FileModeFlags.HL_MODE_WRITE | FileModeFlags.HL_MODE_NO_FILEMAPPING;
+                bool opened = package.Open(file, mode, overwriteFiles: true);
+                if (!opened)
+                {
+                    Console.WriteLine("Package could not be opened!");
+                    Console.WriteLine();
+                    return;
+                }
+
+                // Print all package info
+                Package.PrintPackageInfo(package);
+
+                // Create the root directory
+                DirectoryFolder rootDirectory = package.GetRoot();
+
+                // Extract all files
+                string outputPath = Path.Combine(Path.GetDirectoryName(Path.GetFullPath(file)), Path.GetFileNameWithoutExtension(file));
+                rootDirectory.Extract(outputPath, readEncrypted: true, overwrite: true);
+
+                // Close the package explicitly
+                package.Close();
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine();
             }
         }
 
