d0c7b16ee9bfadf329975fc44392120fa3ff974f
Matt Nadareski <mnadareski@outlook.com>
2022-12-23 13:13:43 -0800

    Add VBSP builder (nw)

--- HLLibSharp/Models/VBSP/Header.cs
+++ HLLibSharp/Models/VBSP/Header.cs
@@ -21,6 +21,11 @@ namespace HLLib.Models.VBSP
         /// <summary>
         /// BSP file version.
         /// </summary>
+        /// <remarks>
+        /// 19-20:			Source
+        /// 21:				Source - The lump version property was moved to the start of the struct.
+        /// 0x00040014:		Dark Messiah - Looks like the 32 bit version has been split into two 16 bit fields.
+        /// </remarks>
         public int Version;
 
         /// <summary>
