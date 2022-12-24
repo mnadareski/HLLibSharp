abd681de6f733b367e4f7dc3b5b78b6b454a650d
Matt Nadareski <mnadareski@outlook.com>
2022-12-23 22:22:36 -0800

    Add WAD builder (nw)

--- HLLibSharp.Models/WAD/LumpInfo.cs
+++ HLLibSharp.Models/WAD/LumpInfo.cs
@@ -13,10 +13,20 @@ namespace HLLib.Models.WAD
 {
     public sealed class LumpInfo
     {
+        public string Name;
+
         public uint Width;
 
         public uint Height;
 
+        public uint PixelOffset;
+
+        // 12 bytes of unknown data
+
+        public byte[] PixelData;
+
         public uint PaletteSize;
+
+        public byte[] PaletteData;
     }
 }
